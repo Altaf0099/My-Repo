9c9b4bb9ff18ec2501f58f24e577671f4723962a
altaf khan <109580622+Altaf0099@users.noreply.github.com>
2023-03-16 14:12:21 +0530

    Add files via upload

---
+++ _1practice.py
@@ -0,0 +1,10 @@
+f1 = input("enter a fruit name 1: ")
+f2 = input("enter a fruit name 2: ")
+f3 = input("enter a fruit name 3: ")
+f4 = input("enter a fruit name 4: ")
+f5 = input("enter a fruit name 5: ")
+f6 = input("enter a fruit name 6: ")
+f7 = input("enter a fruit name 7: ")
+
+myfruitlist = [f1,f2,f3,f4,f5,f6,f7]
+print(myfruitlist)
